23b6257b98f5d32d230bda3c2e97a36f485ac468
bugis <matbugail@gmail.com>
2026-09-18 13:45:06 +0300

    Initial commit, license, readme, starter c++ file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <windows.h>
+#include <iostream>
+#include <cmath>
+
+#define _USE_MATH_DEFINES // del M_PI, M_E, ir kt.
+
+
+using std::cout;
+
+int main(int argc, char* argv[]) {
+    // Set console output code page to UTF-8
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    
+    
+    return 0;
+}
